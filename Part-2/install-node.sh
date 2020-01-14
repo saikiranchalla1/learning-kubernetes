ce1585a24386d9aa990d458d5b483c99b9ab93ba
Sai Kiran Challa <schalla@adobe.com>
2020-01-14 15:44:16 -0700

    updating Docker version

--- Part-2/install-node.sh
+++ Part-2/install-node.sh
@@ -11,7 +11,7 @@ add-apt-repository \
    "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
    $(lsb_release -cs) \
    stable"
-apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
+apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 18.06 | head -1 | awk '{print $3}')
 
 echo "installing kubeadm and kubectl"
 apt-get update && apt-get install -y apt-transport-https
